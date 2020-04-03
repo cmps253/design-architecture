bce6db5a336dc2c7e0c368bf0ccabee4c00e0891
Mahmoud Bdeir <mbdeir@aub.edu.lb>
2020-04-03 10:43:01 +0300

    Fixed #12

--- Lesson 05 Log To File/Solution 0 LogToFile Method/Source Code/Driver/Program.cs
+++ Lesson 05 Log To File/Solution 0 LogToFile Method/Source Code/Driver/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System.Threading;
 
-namespace Lesson5
+namespace Lesson5.Solution0
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
 			Logger.Log("Program Started");
-			FileLogger.LogToFile(".\\log.txt","Program Started");
+			Logger.LogToFile(".\\log.txt","Program Started");
 			
 			Thread.Sleep(3000); //Simulating work by having the program sleep for 3 seconds
 			
 			Logger.Log("Program Ended");
-			FileLogger.LogToFile(".\\log.txt", "Program Ended");
+			Logger.LogToFile(".\\log.txt", "Program Ended");
 		}
 	}
 }
--- Lesson 05 Log To File/Solution 0 LogToFile Method/Source Code/Logger/Logger.cs
+++ Lesson 05 Log To File/Solution 0 LogToFile Method/Source Code/Logger/Logger.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.IO;
 
-namespace Lesson5
+namespace Lesson5.Solution0
 {
     public static class Logger
     {
         public static void Log(string msg)
         {
             Console.WriteLine($"{DateTime.Now} {msg}");
+        }
+        public static void LogToFile(string fileName, string msg)
+        {
+            File.AppendAllText(fileName, $"{DateTime.Now} {msg}\n");
         }
     }
 }
